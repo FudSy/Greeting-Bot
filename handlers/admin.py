20805af53f94c353d6037f069de3564fe5ddbdd2
FudSy <matartevit@gmail.com>
2025-07-01 20:33:29 +0300

    added phrases editing functionality

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.filters import Command, StateFilter
-from aiogram.types import Message, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from config import admins
@@ -28,7 +28,7 @@ async def configure_phrases(message: Message, state:FSMContext):
     await message.answer(text=f"Выберите какую фразу редактировать (1 или 2):\n\n1.{phrases['phrase_1']}\n2.{phrases['phrase_2']}", reply_markup=ReplyKeyboardRemove())
 
 @admin_router.message(Form.phraseNumber, F.text == "1" or "2")
-async def get_phrase(message: Message, state: FSMContext):
+async def get_phrase_from_user(message: Message, state: FSMContext):
     await state.update_data(phraseNumber = message.text)
     await state.set_state(Form.new_text)
     await message.answer(text="Введите новый текст:")
@@ -40,4 +40,14 @@ async def update_phrase(message: Message, state: FSMContext):
     kb = [[InlineKeyboardButton(text="Да", callback_data="yes"), InlineKeyboardButton(text="Нет", callback_data="no")]]
     keyboard = InlineKeyboardMarkup(inline_keyboard=kb)
     await message.answer(text=f"Ваш измененный текст:\n\n{data['new_text']}", reply_markup=keyboard)
+    
+
+@admin_router.callback_query(F.data == "yes")
+async def write_phrase(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    phrases = jsonParser.load_phrases()
+    phrases[f'phrase_{data["phraseNumber"]}'] = data["new_text"]
+    jsonParser.save_phrases(phrases)
+    await callback.message.edit_reply_markup(reply_markup=None)
+    await callback.answer("Текст сохранен")
     await state.clear()
