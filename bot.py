2cd17c3e31ea14e94d65aed25cfb78db5155105d
FudSy <matartevit@gmail.com>
2025-12-03 16:03:50 +0300

    changed handlers priority

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from create_bot import bot, dp
 from handlers import start, admin
 
 async def main():
-    dp.include_router(start.start_router)
     dp.include_router(admin.admin_router)
+    dp.include_router(start.start_router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
